b7940985eee38fa50fe6adaa6576d5c0539b09ee
= <=>
2020-12-23 16:40:51 +0000

    tracks marked values accross debu sessions

--- src/main/java/com/intellij/PostStartupActivity.java
+++ src/main/java/com/intellij/PostStartupActivity.java
@@ -1,24 +1,39 @@
 package com.intellij;
 
+import com.intellij.debugger.engine.JavaDebugProcess;
 import com.intellij.debugger.engine.JavaStackFrame;
 import com.intellij.debugger.engine.evaluation.EvaluateException;
+import com.intellij.debugger.impl.DebuggerContextImpl;
+import com.intellij.debugger.impl.DebuggerContextListener;
+import com.intellij.debugger.impl.DebuggerSession;
+import com.intellij.debugger.ui.DebuggerContentInfo;
+import com.intellij.execution.ui.RunnerLayoutUi;
+import com.intellij.execution.ui.layout.PlaceInGrid;
+import com.intellij.ide.ui.customization.CustomActionsSchema;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.ui.content.Content;
 import com.intellij.xdebugger.*;
 import com.intellij.xdebugger.breakpoints.XBreakpoint;
 import com.intellij.xdebugger.breakpoints.XBreakpointListener;
 import com.intellij.xdebugger.breakpoints.XBreakpointManager;
+import com.intellij.xdebugger.breakpoints.XBreakpointType;
 import com.intellij.xdebugger.frame.*;
 import com.intellij.xdebugger.impl.XDebugSessionImpl;
+import com.intellij.xdebugger.impl.actions.XDebuggerActions;
 import com.intellij.xdebugger.impl.frame.XValueMarkers;
+import com.intellij.xdebugger.impl.frame.XVariablesView;
+import com.intellij.xdebugger.impl.frame.XWatchesViewImpl;
+import com.intellij.xdebugger.impl.ui.tree.ValueMarkup;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,6 +44,13 @@ public class PostStartupActivity implements StartupActivity {
     private static final HashMap<Integer, XBreakpoint> breakPointsMap = new HashMap<>();
     //holds the hit counts for a breakpoint
     private static final HashMap<Integer, Integer> breakPointsHitCounts = new HashMap<>();
+    //current method name been traversed by debugger
+    String methodName = null;
+    RunnerLayoutUi ui;
+    private final boolean myWatchesInVariables = Registry.is("debugger.watches.in.variables");
+    protected XWatchesViewImpl myWatchesView;
+    XValueMarkers<?, ?> markers;
+    Set<XValue> xValueSet = new HashSet<>();
 
     @Override
     public void runActivity(@NotNull Project project) {
@@ -42,6 +64,9 @@ public class PostStartupActivity implements StartupActivity {
 
     private void getCurrentBreakPoints(Project project) {
         XBreakpointManager manager = XDebuggerManager.getInstance(project).getBreakpointManager();
+//       XDebuggerManager.getInstance(project).getCurrentSession().getUI().addListener();
+
+
 
         Arrays.stream(manager.getAllBreakpoints()).forEach(xBreakpoint -> {
             System.out.println( "pre-existing breakpoints");
@@ -102,11 +127,12 @@ public class PostStartupActivity implements StartupActivity {
     private void attachDebugBreakListener(@NotNull XDebugProcess debugProcess) {
         System.out.println( "Attaching session listener");
 
+
         debugProcess.getSession().addSessionListener(new XDebugSessionListener() {
             @Override
             public void sessionPaused() {
                 try {
-                    attachComputeChildrenListener(Objects.requireNonNull(debugProcess.getSession().getCurrentStackFrame()));
+                    attachComputeChildrenListener(Objects.requireNonNull(debugProcess.getSession()));
                 } catch (EvaluateException e) {
                     e.printStackTrace();
                 }
@@ -143,9 +169,9 @@ public class PostStartupActivity implements StartupActivity {
         });
     }
 
-    private void attachComputeChildrenListener(XStackFrame currentStackFrame) throws EvaluateException {
+    private void attachComputeChildrenListener(XDebugSession xDebugSession) throws EvaluateException {
+        XStackFrame currentStackFrame = xDebugSession.getCurrentStackFrame();
         System.out.println( "Current stack frame");
-
 
         System.out.println( "Frame stack file");
         System.out.println( Objects.requireNonNull(currentStackFrame.getSourcePosition()).getFile().toString());
@@ -154,9 +180,9 @@ public class PostStartupActivity implements StartupActivity {
         System.out.println( "method name: " + method);
         System.out.println( "Stack position");
         int currentStackLine = Objects.requireNonNull(currentStackFrame.getSourcePosition()).getLine();
-        System.out.println( String.valueOf(currentStackLine));
+        System.out.println(currentStackLine);
 
-
+        handleMarkedObjectPresentationToUI(xDebugSession, method);
         displayAllBreakpointsAndTrack(currentStackLine);
 
         Objects.requireNonNull(currentStackFrame).computeChildren(new XCompositeNode() {
@@ -164,6 +190,8 @@ public class PostStartupActivity implements StartupActivity {
             public void addChildren(@NotNull XValueChildrenList children, boolean last) {
                 for (int c = 0; c < children.size(); c++) {
                     XValue childValue = children.getValue(c);
+                    boolean isAdded = xValueSet.add(childValue);
+                    System.out.println("Is added to set " + isAdded);
                     System.out.println( "Scope Variable:  " + childValue.toString());
                 }
             }
@@ -198,6 +226,71 @@ public class PostStartupActivity implements StartupActivity {
 
             }
         });
+    }
+
+    private void handleMarkedObjectPresentationToUI(XDebugSession xDebugSession, String method) {
+        //check and see if function method currently traversed by the debugger is in a different method
+        //from the previous one, if true, modify the debugger variables view to display our saved marked objects
+        if(methodName != null && !methodName.equals(method)) {
+            //we are in a different method
+            System.out.println("We are in a different method");
+             ui = xDebugSession.getUI();
+
+             //list all the xValues
+            if(markers == null) return;
+            System.out.println("About attaching marked objects");
+            xValueSet.forEach(xValue -> {
+                System.out.println("**********************************************************");
+                System.out.println("Getting mark up for " + xValue.toString());
+                ValueMarkup valueMarkup = markers.getMarkup(xValue);
+                if(valueMarkup != null) {
+                    System.out.println("Markup " + valueMarkup.toString());
+                    ((XDebugSessionImpl) xDebugSession).getValueMarkers().markValue(xValue, valueMarkup);
+                    System.out.println("Finished attaching markup");
+                }
+                System.out.println("**********************************************************");
+            });
+
+            //todo: invoke on a separate thread
+            ui.addContent(createVariablesContent((XDebugSessionImpl) xDebugSession), 0, PlaceInGrid.center, false);
+            if (!myWatchesInVariables) {
+                ui.addContent(createWatchesContent((XDebugSessionImpl) xDebugSession), 0, PlaceInGrid.right, false);
+            }
+
+//            xDebugSession.rebuildViews();
+        }
+        methodName = method;
+    }
+
+    private Content createWatchesContent(@NotNull XDebugSessionImpl session) {
+        myWatchesView = new XWatchesViewImpl(session, myWatchesInVariables);
+//        registerView(DebuggerContentInfo.WATCHES_CONTENT, myWatchesView);
+        Content watchesContent = ui.createContent(DebuggerContentInfo.WATCHES_CONTENT, myWatchesView.getPanel(),
+                XDebuggerBundle.message("debugger.session.tab.watches.title"), null, myWatchesView.getDefaultFocusedComponent());
+        watchesContent.setCloseable(false);
+        return watchesContent;
+    }
+
+    private Content createVariablesContent(@NotNull XDebugSessionImpl session) {
+        XVariablesView variablesView;
+        if (myWatchesInVariables) {
+            variablesView = myWatchesView = new XWatchesViewImpl(session, myWatchesInVariables);
+        } else {
+            variablesView = new XVariablesView(session);
+        }
+//        registerView(DebuggerContentInfo.VARIABLES_CONTENT, variablesView);
+        Content result = ui.createContent(DebuggerContentInfo.VARIABLES_CONTENT, variablesView.getPanel(),
+                XDebuggerBundle.message("debugger.session.tab.variables.title"),
+                null, variablesView.getDefaultFocusedComponent());
+        result.setCloseable(false);
+
+        ActionGroup group = getCustomizedActionGroup(XDebuggerActions.VARIABLES_TREE_TOOLBAR_GROUP);
+        result.setActions(group, ActionPlaces.DEBUGGER_TOOLBAR, variablesView.getTree());
+        return result;
+    }
+
+    public static ActionGroup getCustomizedActionGroup(final String id) {
+        return (ActionGroup) CustomActionsSchema.getInstance().getCorrectedAction(id);
     }
 
     private void addBreakPoint(XBreakpoint breakpoint) {
@@ -251,15 +344,13 @@ public class PostStartupActivity implements StartupActivity {
     }
 
     private void trackMarkedObjects(XDebugProcess debugProcess) {
-        XValueMarkers<?, ?> markers = ((XDebugSessionImpl) debugProcess.getSession()).getValueMarkers();
-        //todo: hold the marked objects in a hashmap
+        //todo: improve logic to hold all incoming markers, not replace the already existing markers
+      markers = ((XDebugSessionImpl) debugProcess.getSession()).getValueMarkers();
         System.out.println( "Marked Objects");
 
         markers.getAllMarkers().forEach((o, valueMarkup) -> {
             System.out.println( o.toString());
             System.out.println( valueMarkup.getText());
-            System.out.println( valueMarkup.getToolTipText());
-            System.out.println( valueMarkup.getColor().toString());
         });
     }
 }
